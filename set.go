a886b2d2b0d26200213e6b470deed70a37e9669b
Matthieu <matthieu.cloet@wattearth.com>
2022-08-08 17:14:42 +0200

    add documentation to the Set interface

--- set.go
+++ set.go
@@ -2,8 +2,10 @@ package coffee
 
 type Set[T comparable] interface {
 	// Adds the specified element to this set if it is not already present.
+	// Returns true if this set did not already contain the specified element
 	Add(T) bool
 	// Adds all of the elements in the specified collection to this set if they're not already present.
+	// Returns true if this set changed as a result of the call
 	AddAll([]T) bool
 	// Removes all of the elements from this set.
 	Clear()
@@ -18,9 +20,12 @@ type Set[T comparable] interface {
 	// Returns an array containing all of the elements in this set.
 	ToArray() []T
 	// Removes the specified element from this set if it is present.
+	// Returns true if this set contained the specified element
 	Remove(T) bool
 	// Removes from this set all of its elements that are contained in the specified collection.
+	// Returns true if this set changed as a result of the call
 	RemoveAll([]T) bool
 	// Retains only the elements in this set that are contained in the specified collection.
+	// Returns true if this set changed as a result of the call
 	RetainAll([]T) bool
 }
